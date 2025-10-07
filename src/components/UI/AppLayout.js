31bec74af6f93b8d745cdf7e10824681affb1c78
Bhabin Chudal <chudalbhabin26@gmail.com>
2025-10-06 23:43:12 -0300

    Modal window added

--- src/components/UI/AppLayout.js
+++ src/components/UI/AppLayout.js
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Navigation from "./Navigation";
 import Footer from "./Footer";
 import { useState } from "react";
+import Modal2 from "./Modal2";
 const AppLayout = () => {
   const [dark, setDark] = useState(false); // State to manage dark mode
 
@@ -15,6 +16,14 @@ const AppLayout = () => {
     <div className="bg-yellow-100 dark:bg-blue-900 min-h-screen">
       <Navigation toggleDarkMode={darkModeHandler} dark={dark} />
       <Outlet />
+      <Modal2>
+        <Modal2.Open name="createForm">
+          <button>click me</button>
+        </Modal2.Open>
+        <Modal2.Window name="createForm">
+          <Footer />
+        </Modal2.Window>
+      </Modal2>
       <Footer />
     </div>
   );
