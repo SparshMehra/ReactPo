a3279e4b9951bc8288b45990e8e4e996da07779b
Susmita Oli <147460379+sadikshyaoli@users.noreply.github.com>
2025-12-12 23:22:23 -0400

    Add author information to header.cy.js
    Added author information to the header test file.

--- cypress/e2e/header.cy.js
+++ cypress/e2e/header.cy.js
@@ -1,4 +1,5 @@
 // cypress/e2e/header.cy.js
+//Abdiaziz Muse — A00471783 
 describe("Header Navigation Test", () => {
   it("Navigates to the Events page when the Events link is clicked", () => {
     // Use the UGDEV URL for grading; change to localhost when testing locally
